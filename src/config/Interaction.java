800fea271012c59834a5d041147ac62960efddc9
HOARAU Yael <h14010671@a-maq-iutinfo.salsa.univ-amu.fr>
2019-03-04 10:09:30 +0100

    Correction of enum initilize

--- src/config/Interaction.java
+++ src/config/Interaction.java
@@ -8,32 +8,35 @@ import lib.GameLayout;
 import lib.Inventory;
 
 public enum Interaction {
-    PNJ((EventHandler<KeyEvent>) event -> {
-        if(event.getCode() == KeyboardConfig.ENTER.getKey().getKeyCode()){
-            DialogLayout.getINSTANCE().setText("I'M A PNJ");
-            System.out.println("I'M A PNJ");
-            InventoryConfig inventoryConfig = InventoryConfig.OBJ1;
-            Inventory.getINSTANCE().add(inventoryConfig);
-        }
-        Interaction i = Interaction.PNJ;
-        GameLayout.getSCENE().removeEventHandler(KeyEvent.KEY_PRESSED, i.getEventHandler());
-    }),
-    PNJ2((EventHandler<KeyEvent>) event -> {
-        if(event.getCode() == KeyboardConfig.ENTER.getKey().getKeyCode()){
-            DialogLayout.getINSTANCE().setText("I'M A PNJ");
-            System.out.println("I'M A PNJ");
-            InventoryConfig inventoryConfig = InventoryConfig.OBJ3;
-            Inventory.getINSTANCE().add(inventoryConfig);
-        }
-        Interaction i = Interaction.PNJ2;
-        GameLayout.getSCENE().removeEventHandler(KeyEvent.KEY_PRESSED, i.getEventHandler());
-    });
+    PNJ,
+    PNJ2;
+
+    static {
+        PNJ.eventHandler = ((EventHandler<KeyEvent>) event -> {
+            if(event.getCode() == KeyboardConfig.ENTER.getKey().getKeyCode()){
+                DialogLayout.getINSTANCE().setText("I'M A PNJ");
+                System.out.println("I'M A PNJ");
+                InventoryConfig inventoryConfig = InventoryConfig.OBJ1;
+                Inventory.getINSTANCE().add(inventoryConfig);
+            }
+            Interaction i = Interaction.PNJ;
+            GameLayout.getSCENE().removeEventHandler(KeyEvent.KEY_PRESSED, i.getEventHandler());
+        });
+        PNJ2.eventHandler = ((EventHandler<KeyEvent>) event -> {
+            if(event.getCode() == KeyboardConfig.ENTER.getKey().getKeyCode()){
+                DialogLayout.getINSTANCE().setText("I'M A PNJ");
+                System.out.println("I'M A PNJ");
+                InventoryConfig inventoryConfig = InventoryConfig.OBJ3;
+                Inventory.getINSTANCE().add(inventoryConfig);
+            }
+            Interaction i = Interaction.PNJ2;
+            GameLayout.getSCENE().removeEventHandler(KeyEvent.KEY_PRESSED, i.getEventHandler());
+        });
+    }
 
     private EventHandler eventHandler;
 
-    Interaction(EventHandler eventHandler) {
-        this.eventHandler = eventHandler;
-    }
+    Interaction() {}
 
     public EventHandler getEventHandler() {
         return eventHandler;
