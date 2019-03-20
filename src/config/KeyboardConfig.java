4a22e48eadc69de31fcbeb773f6928d32cd91ff2
Yaël Hoarau <yael.hoarau@etu.univ-amu.fr>
2019-03-21 00:04:02 +0100

    Bouton actionables avec Entrée

--- src/config/KeyboardConfig.java
+++ src/config/KeyboardConfig.java
@@ -1,36 +1,42 @@
 package config;
 
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import lib.*;
-import sample.Main;
+
 
 public enum KeyboardConfig {
 
     ENTER(Key.ENTER, (EventHandler<KeyEvent>) event -> {
         if(event.getCode() == Key.ENTER.getKeyCode()){
-            boolean isAPNJ = false;
-            BlockingCell blockingCellInteract = null;
-            Player player = Player.getINSTANCE();
-            int col = player.getDirection() == Direction.RIGHT ? player.getPosition().getKey() + 1 :
-                    (player.getDirection() == Direction.LEFT ? player.getPosition().getKey() - 1 : player.getPosition().getKey());
-            int row = player.getDirection() == Direction.DOWN ? player.getPosition().getValue() + 1 :
-                    (player.getDirection() == Direction.UP ? player.getPosition().getValue() - 1 : player.getPosition().getValue());
-            while(Movement.getMap().getBlockingCellIterator().hasNext()){
-                BlockingCell blockingCell = Movement.getMap().getBlockingCellIterator().next();
-                if(blockingCell.getPosition().getKey().equals(col) && blockingCell.getPosition().getValue().equals(row)
-                        && blockingCell.getInteraction() != null ){
-                    Movement.getMap().getBlockingCellIterator().reset();
-                    isAPNJ = true;
-                    blockingCellInteract = blockingCell;
-                    break;
+            if(GameLayout.getINSTANCE().getPane().isFocused()){
+                boolean isAPNJ = false;
+                BlockingCell blockingCellInteract = null;
+                Player player = Player.getINSTANCE();
+                int col = player.getDirection() == Direction.RIGHT ? player.getPosition().getKey() + 1 :
+                        (player.getDirection() == Direction.LEFT ? player.getPosition().getKey() - 1 : player.getPosition().getKey());
+                int row = player.getDirection() == Direction.DOWN ? player.getPosition().getValue() + 1 :
+                        (player.getDirection() == Direction.UP ? player.getPosition().getValue() - 1 : player.getPosition().getValue());
+                while(Movement.getMap().getBlockingCellIterator().hasNext()){
+                    BlockingCell blockingCell = Movement.getMap().getBlockingCellIterator().next();
+                    if(blockingCell.getPosition().getKey().equals(col) && blockingCell.getPosition().getValue().equals(row)
+                            && blockingCell.getInteraction() != null ){
+                        Movement.getMap().getBlockingCellIterator().reset();
+                        isAPNJ = true;
+                        blockingCellInteract = blockingCell;
+                        break;
+                    }
+                }
+                if(isAPNJ){
+                    MainLayout.getSCENE().addEventHandler(KeyEvent.KEY_PRESSED, blockingCellInteract.getInteraction().getEventHandler());
+                }
+                else{
+                    DialogLayout.getINSTANCE().setText("YOUPI");
                 }
             }
-            if(isAPNJ){
-                MainLayout.getSCENE().addEventHandler(KeyEvent.KEY_PRESSED, blockingCellInteract.getInteraction().getEventHandler());
-            }
-            else{
-                DialogLayout.getINSTANCE().setText("YOUPI");
+            else if(MainLayout.getSCENE().getFocusOwner() instanceof Button){
+                ((Button) MainLayout.getSCENE().getFocusOwner()).fire();
             }
         }
     }),
