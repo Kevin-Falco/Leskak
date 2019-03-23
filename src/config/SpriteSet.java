9f3ec8f939102fd55e7654cf25606dd3d68c60bf
Yaël Hoarau <yael.hoarau@etu.univ-amu.fr>
2019-03-23 19:57:43 +0100

    Complétion des SpriteSet existants

--- src/config/SpriteSet.java
+++ src/config/SpriteSet.java
@@ -1,6 +1,5 @@
 package config;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,10 +7,10 @@ import java.util.List;
 public enum SpriteSet {
     TREE_SET(new ArrayList<>(Arrays.asList(Sprite.TREE, Sprite.TREE_UP, Sprite.TREE_DOWN, Sprite.TREE_RIGHT, Sprite.TREE_LEFT,
             Sprite.TREE_UP_RIGHT, Sprite.TREE_UP_LEFT, Sprite.TREE_UP_LEFT_RIGHT, Sprite.TREE_DOWN_RIGHT, Sprite.TREE_DOWN_LEFT,
-            Sprite.TREE_DOWN_LEFT_RIGHT, Sprite.TREE_RIGHT_UP_DOWN, Sprite.TREE_LEFT_UP_DOWN ))),
+            Sprite.TREE_DOWN_LEFT_RIGHT, Sprite.TREE_RIGHT_UP_DOWN, Sprite.TREE_LEFT_UP_DOWN, Sprite.TREE_UP_DOWN, Sprite.TREE_LEFT_RIGHT ))),
     WATER_SET(new ArrayList<>(Arrays.asList(Sprite.WATER, Sprite.WATER_UP, Sprite.WATER_DOWN, Sprite.WATER_RIGHT, Sprite.WATER_LEFT,
              Sprite.WATER_UP_RIGHT, Sprite.WATER_UP_LEFT, Sprite.WATER_UP_LEFT_RIGHT, Sprite.WATER_DOWN_RIGHT, Sprite.WATER_DOWN_LEFT,
-             Sprite.WATER_DOWN_LEFT_RIGHT, Sprite.WATER_RIGHT_UP_DOWN, Sprite.WATER_LEFT_UP_DOWN )));
+             Sprite.WATER_DOWN_LEFT_RIGHT, Sprite.WATER_RIGHT_UP_DOWN, Sprite.WATER_LEFT_UP_DOWN, Sprite.WATER_UP_DOWN, Sprite.WATER_LEFT_RIGHT )));
 
     private List<Sprite> sprites;
 
@@ -37,5 +36,7 @@ public enum SpriteSet {
     public Sprite getDownLeftRight(){ return sprites.get(10); }
     public Sprite getRightUpDown(){ return sprites.get(11); }
     public Sprite getLeftUpDown(){ return sprites.get(12); }
+    public Sprite getUpDown(){ return sprites.get(13); }
+    public Sprite getLeftRight(){ return sprites.get(14); }
 
 }
