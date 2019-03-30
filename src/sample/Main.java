db488ea16d47365da1496e3dc616b24ab007783a
f16000576 <kevin.falco@etu.univ-amu.fr>
2019-03-30 15:43:07 +0100

    Documentation terminée

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,12 +7,22 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import lib.LauncherLayout;
 
-
+/**
+ * Classe principale du jeu, permettant le lancement de l'application JavaFX.
+ */
 public class Main extends Application {
+
+    /**
+     * Principale Stage du jeu.
+     */
     private static Stage stage;
 
+    /**
+     * Configure tout ce qu'il faut pour que le jeu apparaisse à l'écran avec une fenêtre de jeu personnalisée.
+     * @param primaryStage Stage principale du jeu
+     */
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         stage = primaryStage;
         primaryStage.setTitle("Leskak");
         Scene scene = LauncherLayout.getSCENE();
@@ -22,10 +32,18 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * Getter du Stage principal du jeu.
+     * @return Stage
+     */
     public static Stage getStage() {
-        return stage;
+        return Main.stage;
     }
 
+    /**
+     * Permet le lancement du jeu, fonction main.
+     * @param args arguments du main
+     */
     public static void main(String[] args) {
         launch(args);
     }
